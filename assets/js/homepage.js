be49895b5dcf4f679fae5bb27eb592f7f553dae1
joshuad1590 <joshuad15@gmail.com>
2022-03-13 12:59:49 -0400

    search repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,7 +3,22 @@ var userFormEl = document.querySelector('#user-form');
 var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector('#repos-container');
 var repoSearchTerm = document.querySelector('#repo-search-term');
+var languageButtonsEl = document.querySelector("#language-buttons");
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+          response.json().then(function(data) {
+            displayRepos(data.items, language);
+          });
+        } else {
+          alert('Error: GitHub User Not Found');
+        }
+      });
+    
+  };
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
   event.preventDefault();
@@ -92,5 +107,17 @@ for (var i = 0; i < repos.length; i++) {
   }
 };
 
+var buttonClickHandler = function (event) {
+    var language = event.target.getAttribute("data-language");
+    if (language) {
+        getFeaturedRepos(language);
+      
+        // clear old content
+        repoContainerEl.textContent = "";
+      }
+    console.log(language);
+}
+
 // add event listeners to forms
 userFormEl.addEventListener('submit', formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
